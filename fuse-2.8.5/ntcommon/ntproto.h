25c773d8f56dd26fae740f7838a7a5f3db34320c
konrad <konrad@0ad51bf3-3eb5-41bf-9b2c-be99ea3c4cf2>
2011-04-23 07:09:29 +0000

    make ntproto.h consistant with fuse formatting
    git-svn-id: file:///home/konrad/curqtr/cse481a/schooldir/repo@11 0ad51bf3-3eb5-41bf-9b2c-be99ea3c4cf2

--- fuse-2.8.5/ntcommon/ntproto.h
+++ fuse-2.8.5/ntcommon/ntproto.h
@@ -6,49 +6,49 @@
 // Requests from Kernel to Userspace
 
 typedef struct FUSENT_GENERIC_REQ {
-  PIRP pirp;
-  PFILE_OBJECT fop;
-  IRP irp;
-  uint8_t irpstack[0];
+	PIRP pirp;
+	PFILE_OBJECT fop;
+	IRP irp;
+	uint8_t irpstack[0];
 };
 
 typedef struct FUSENT_CREATE_REQ {
-  PIRP pirp;
-  PFILE_OBJECT fop;
-  IRP irp;
-  uint32_t fnamelen; // in bytes
-  uint8_t rem[0]; // irp stack, followed by fnamelen
-                  // bytes of UTF-16LE file name
+	PIRP pirp;
+	PFILE_OBJECT fop;
+	IRP irp;
+	uint32_t fnamelen; // in bytes
+	uint8_t rem[0]; // irp stack, followed by fnamelen
+	// bytes of UTF-16LE file name
 };
 
 typedef struct FUSENT_WRITE_REQ {
-  PIRP pirp;
-  PFILE_OBJECT fop;
-  IRP irp;
-  uint32_t buflen;
-  uint8_t rem[0]; // irp stack, followed by buflen bytes of write data
+	PIRP pirp;
+	PFILE_OBJECT fop;
+	IRP irp;
+	uint32_t buflen;
+	uint8_t rem[0]; // irp stack, followed by buflen bytes of write data
 };
 
 // Responses (Userspace to Kernelspace)
 
 typedef struct FUSENT_MOUNT { // weird special-case "response"
-  uint32_t mtptlen; // in bytes
-  uint32_t mtoptslen; // in bytes
-  uint8_t rem[0]; // mtptlen bytes of UTF-16LE mount path,
-                  // followed by mtoptslen bytes of mount options
-                  // (mtptlen should be word-sized and nul-padded to make
-                  // mtopts aligned)
+	uint32_t mtptlen; // in bytes
+	uint32_t mtoptslen; // in bytes
+	uint8_t rem[0]; // mtptlen bytes of UTF-16LE mount path,
+	// followed by mtoptslen bytes of mount options
+	// (mtptlen should be word-sized and nul-padded to make
+	// mtopts aligned)
 };
 
 typedef struct FUSENT_GENERIC_RESP {
-  PIRP pirp;
-  PFILE_OBJECT fop;
-  int retval; // all high-level fuse operations return int
-  union {
-    struct {
-      uint32_t buflen;
-      uint8_t buf[0];
-    } read;
-    // potentially other kinds of responses here...
-  } extradata;
+	PIRP pirp;
+	PFILE_OBJECT fop;
+	int retval; // all high-level fuse operations return int
+	union {
+		struct {
+			uint32_t buflen;
+			uint8_t buf[0];
+		} read;
+		// potentially other kinds of responses here...
+	} extradata;
 };
