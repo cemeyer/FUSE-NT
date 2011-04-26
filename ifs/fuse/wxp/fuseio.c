f8262fe95e403b6c80c823ebe0d796652524cb5a
snowden <snowden@0ad51bf3-3eb5-41bf-9b2c-be99ea3c4cf2>
2011-04-26 07:11:39 +0000

    Add fuseio.c
    git-svn-id: file:///home/konrad/curqtr/cse481a/schooldir/repo@15 0ad51bf3-3eb5-41bf-9b2c-be99ea3c4cf2

---
+++ ifs/fuse/wxp/fuseio.c
@@ -0,0 +1,510 @@
+/*++
+
+Copyright (c) 2011 FoW Group
+
+Module Name:
+
+    FuseIo.c
+
+Abstract:
+
+    This module implements the main routines for the FUSE filesystem driver
+
+--*/
+
+#include "fuseprocs.h"
+
+NTSTATUS
+FuseMount (
+    IN PVOLUME_DEVICE_OBJECT VolumeDeviceObject,
+    IN PIRP Irp
+    )
+{
+    NTSTATUS Status;
+
+    DbgPrint("FuseMount\n");
+
+    Status = STATUS_SUCCESS;
+    IoCompleteRequest(Irp, IO_DISK_INCREMENT);
+
+    return Status;
+}
+
+NTSTATUS
+FuseRequest (
+    IN PVOLUME_DEVICE_OBJECT VolumeDeviceObject,
+    IN PIRP Irp
+    )
+{
+    NTSTATUS Status;
+
+    DbgPrint("FuseRequest\n");
+
+    Status = STATUS_SUCCESS;
+    IoCompleteRequest(Irp, IO_DISK_INCREMENT);
+
+    return Status;
+}
+
+
+NTSTATUS
+FuseFsdCleanup (
+    IN PVOLUME_DEVICE_OBJECT VolumeDeviceObject,
+    IN PIRP Irp
+    )
+{
+	return STATUS_CLEANER_CARTRIDGE_INSTALLED;
+}
+
+NTSTATUS
+FuseFsdClose (
+    IN PVOLUME_DEVICE_OBJECT VolumeDeviceObject,
+    IN PIRP Irp
+    )
+{
+    DbgPrint("FuseFsdClose\n");
+	return STATUS_CLEANER_CARTRIDGE_INSTALLED;
+}
+
+NTSTATUS
+FuseFsdCreate (
+    IN PVOLUME_DEVICE_OBJECT VolumeDeviceObject,
+    IN PIRP Irp
+    )
+{
+    DbgPrint("FuseFsdCreate\n");
+	return STATUS_CLEANER_CARTRIDGE_INSTALLED;
+}
+
+NTSTATUS
+FuseFsdRead (
+    IN PVOLUME_DEVICE_OBJECT VolumeDeviceObject,
+    IN PIRP Irp
+    )
+{
+    DbgPrint("FuseFsdRead\n");
+	return STATUS_CLEANER_CARTRIDGE_INSTALLED;
+}
+
+NTSTATUS
+FuseFsdWrite (
+    IN PVOLUME_DEVICE_OBJECT VolumeDeviceObject,
+    IN PIRP Irp
+    )
+{
+    DbgPrint("FuseFsdWrite\n");
+	return STATUS_CLEANER_CARTRIDGE_INSTALLED;
+}
+
+NTSTATUS
+FuseFsdDeviceControl (
+    IN PVOLUME_DEVICE_OBJECT VolumeDeviceObject,
+    IN PIRP Irp
+    )
+{
+	return STATUS_CLEANER_CARTRIDGE_INSTALLED;
+}
+
+NTSTATUS
+FuseFsdDirectoryControl (
+    IN PVOLUME_DEVICE_OBJECT VolumeDeviceObject,
+    IN PIRP Irp
+    )
+{
+	return STATUS_CLEANER_CARTRIDGE_INSTALLED;
+}
+
+NTSTATUS
+FuseFsdQueryEa (
+    IN PVOLUME_DEVICE_OBJECT VolumeDeviceObject,
+    IN PIRP Irp
+    )
+{
+	return STATUS_CLEANER_CARTRIDGE_INSTALLED;
+}
+
+NTSTATUS
+FuseFsdSetEa (
+    IN PVOLUME_DEVICE_OBJECT VolumeDeviceObject,
+    IN PIRP Irp
+    )
+{
+	return STATUS_CLEANER_CARTRIDGE_INSTALLED;
+}
+
+NTSTATUS
+FuseFsdQueryInformation (
+    IN PVOLUME_DEVICE_OBJECT VolumeDeviceObject,
+    IN PIRP Irp
+    )
+{
+	return STATUS_CLEANER_CARTRIDGE_INSTALLED;
+}
+
+NTSTATUS
+FuseFsdSetInformation (
+    IN PVOLUME_DEVICE_OBJECT VolumeDeviceObject,
+    IN PIRP Irp
+    )
+{
+	return STATUS_CLEANER_CARTRIDGE_INSTALLED;
+}
+
+NTSTATUS
+FuseFsdFlushBuffers (
+    IN PVOLUME_DEVICE_OBJECT VolumeDeviceObject,
+    IN PIRP Irp
+    )
+{
+	return STATUS_CLEANER_CARTRIDGE_INSTALLED;
+}
+
+NTSTATUS
+FuseFsdFileSystemControl (
+    IN PVOLUME_DEVICE_OBJECT VolumeDeviceObject,
+    IN PIRP Irp
+    )
+{
+	return STATUS_CLEANER_CARTRIDGE_INSTALLED;
+}
+
+NTSTATUS
+FuseFsdLockControl (
+    IN PVOLUME_DEVICE_OBJECT VolumeDeviceObject,
+    IN PIRP Irp
+    )
+{
+	return STATUS_CLEANER_CARTRIDGE_INSTALLED;
+}
+
+NTSTATUS
+FuseFsdPnp (
+    IN PVOLUME_DEVICE_OBJECT VolumeDeviceObject,
+    IN PIRP Irp
+    )
+{
+	return STATUS_CLEANER_CARTRIDGE_INSTALLED;
+}
+
+NTSTATUS
+FuseFsdShutdown (
+    IN PVOLUME_DEVICE_OBJECT VolumeDeviceObject,
+    IN PIRP Irp
+    )
+{
+	return STATUS_CLEANER_CARTRIDGE_INSTALLED;
+}
+
+NTSTATUS
+FuseFsdQueryVolumeInformation (
+    IN PVOLUME_DEVICE_OBJECT VolumeDeviceObject,
+    IN PIRP Irp
+    )
+{
+	return STATUS_CLEANER_CARTRIDGE_INSTALLED;
+}
+
+NTSTATUS
+FuseFsdSetVolumeInformation (
+    IN PVOLUME_DEVICE_OBJECT VolumeDeviceObject,
+    IN PIRP Irp
+    )
+{
+	return STATUS_CLEANER_CARTRIDGE_INSTALLED;
+}
+
+NTSTATUS
+FuseCommonCleanup (
+    IN PIRP_CONTEXT IrpContext,
+    IN PIRP Irp
+    )
+{
+    return STATUS_CLEANER_CARTRIDGE_INSTALLED;
+}
+
+NTSTATUS
+FuseCommonClose (
+    IN PVCB Vcb,
+    IN PFCB Fcb,
+    IN TYPE_OF_OPEN TypeOfOpen,
+    IN BOOLEAN Wait,
+    OUT PBOOLEAN VcbDeleted OPTIONAL
+    )
+{
+    return STATUS_CLEANER_CARTRIDGE_INSTALLED;
+}
+
+VOID
+FuseFspClose (   
+    IN PVCB Vcb OPTIONAL
+    )
+{
+
+}
+
+NTSTATUS
+FuseCommonCreate (
+    IN PIRP_CONTEXT IrpContext,
+    IN PIRP Irp
+    )
+{
+    return STATUS_CLEANER_CARTRIDGE_INSTALLED;
+}
+
+NTSTATUS
+FuseCommonDirectoryControl (
+    IN PIRP_CONTEXT IrpContext,
+    IN PIRP Irp
+    )
+{
+    return STATUS_CLEANER_CARTRIDGE_INSTALLED;
+}
+
+NTSTATUS
+FuseCommonDeviceControl (
+    IN PIRP_CONTEXT IrpContext,
+    IN PIRP Irp
+    )
+{
+    return STATUS_CLEANER_CARTRIDGE_INSTALLED;
+}
+
+NTSTATUS
+FuseCommonQueryEa (
+    IN PIRP_CONTEXT IrpContext,
+    IN PIRP Irp
+    )
+{
+    return STATUS_CLEANER_CARTRIDGE_INSTALLED;
+}
+
+NTSTATUS
+FuseCommonSetEa (
+    IN PIRP_CONTEXT IrpContext,
+    IN PIRP Irp
+    )
+{
+    return STATUS_CLEANER_CARTRIDGE_INSTALLED;
+}
+
+NTSTATUS
+FuseCommonQueryInformation (
+    IN PIRP_CONTEXT IrpContext,
+    IN PIRP Irp
+    )
+{
+    return STATUS_CLEANER_CARTRIDGE_INSTALLED;
+}
+
+NTSTATUS
+FuseCommonSetInformation (
+    IN PIRP_CONTEXT IrpContext,
+    IN PIRP Irp
+    )
+{
+    return STATUS_CLEANER_CARTRIDGE_INSTALLED;
+}
+
+NTSTATUS
+FuseCommonFlushBuffers (
+    IN PIRP_CONTEXT IrpContext,
+    IN PIRP Irp
+    )
+{
+    return STATUS_CLEANER_CARTRIDGE_INSTALLED;
+}
+
+NTSTATUS
+FuseCommonFileSystemControl (
+    IN PIRP_CONTEXT IrpContext,
+    IN PIRP Irp
+    )
+{
+    return STATUS_CLEANER_CARTRIDGE_INSTALLED;
+}
+
+NTSTATUS
+FuseCommonLockControl (
+    IN PIRP_CONTEXT IrpContext,
+    IN PIRP Irp
+    )
+{
+    return STATUS_CLEANER_CARTRIDGE_INSTALLED;
+}
+
+NTSTATUS
+FuseCommonPnp (  
+    IN PIRP_CONTEXT IrpContext,
+    IN PIRP Irp
+    )
+{
+    return STATUS_CLEANER_CARTRIDGE_INSTALLED;
+}
+
+NTSTATUS
+FuseCommonRead ( 
+    IN PIRP_CONTEXT IrpContext,
+    IN PIRP Irp
+    )
+{
+    return STATUS_CLEANER_CARTRIDGE_INSTALLED;
+}
+
+NTSTATUS
+FuseCommonShutdown (
+    IN PIRP_CONTEXT IrpContext,
+    IN PIRP Irp
+    )
+{
+    return STATUS_CLEANER_CARTRIDGE_INSTALLED;
+}
+
+NTSTATUS
+FuseCommonQueryVolumeInfo (
+    IN PIRP_CONTEXT IrpContext,
+    IN PIRP Irp
+    )
+{
+    return STATUS_CLEANER_CARTRIDGE_INSTALLED;
+}
+
+NTSTATUS
+FuseCommonSetVolumeInfo (
+    IN PIRP_CONTEXT IrpContext,
+    IN PIRP Irp
+    )
+{
+    return STATUS_CLEANER_CARTRIDGE_INSTALLED;
+}
+
+NTSTATUS
+FuseCommonWrite (
+    IN PIRP_CONTEXT IrpContext,
+    IN PIRP Irp
+    )
+{
+    return STATUS_CLEANER_CARTRIDGE_INSTALLED;
+}
+
+BOOLEAN
+FuseFastIoCheckIfPossible (
+    IN PFILE_OBJECT FileObject,
+    IN PLARGE_INTEGER FileOffset,
+    IN ULONG Length,
+    IN BOOLEAN Wait,
+    IN ULONG LockKey,
+    IN BOOLEAN CheckForReadOperation,
+    OUT PIO_STATUS_BLOCK IoStatus,
+    IN PDEVICE_OBJECT DeviceObject
+    )
+{
+	return TRUE;
+}
+
+BOOLEAN
+FuseFastQueryBasicInfo (
+    IN PFILE_OBJECT FileObject,
+    IN BOOLEAN Wait,
+    IN OUT PFILE_BASIC_INFORMATION Buffer,
+    OUT PIO_STATUS_BLOCK IoStatus,
+    IN PDEVICE_OBJECT DeviceObject
+    )
+{
+	return TRUE;
+}
+
+BOOLEAN
+FuseFastQueryStdInfo (
+    IN PFILE_OBJECT FileObject,
+    IN BOOLEAN Wait,
+    IN OUT PFILE_STANDARD_INFORMATION Buffer,
+    OUT PIO_STATUS_BLOCK IoStatus,
+    IN PDEVICE_OBJECT DeviceObject
+    )
+{
+	return TRUE;
+}
+
+BOOLEAN
+FuseFastQueryNetworkOpenInfo (
+    IN PFILE_OBJECT FileObject,
+    IN BOOLEAN Wait,
+    IN OUT PFILE_NETWORK_OPEN_INFORMATION Buffer,
+    OUT PIO_STATUS_BLOCK IoStatus,
+    IN PDEVICE_OBJECT DeviceObject
+    )
+{
+	return TRUE;
+}
+
+BOOLEAN
+FuseFastLock (
+    IN PFILE_OBJECT FileObject,
+    IN PLARGE_INTEGER FileOffset,
+    IN PLARGE_INTEGER Length,
+    PEPROCESS ProcessId,
+    ULONG Key,
+    BOOLEAN FailImmediately,
+    BOOLEAN ExclusiveLock,
+    OUT PIO_STATUS_BLOCK IoStatus,
+    IN PDEVICE_OBJECT DeviceObject
+    )
+{
+	return TRUE;
+}
+
+BOOLEAN
+FuseFastUnlockSingle (
+    IN PFILE_OBJECT FileObject,
+    IN PLARGE_INTEGER FileOffset,
+    IN PLARGE_INTEGER Length,
+    PEPROCESS ProcessId,
+    ULONG Key,
+    OUT PIO_STATUS_BLOCK IoStatus,
+    IN PDEVICE_OBJECT DeviceObject
+    )
+{
+	return TRUE;
+}
+
+BOOLEAN
+FuseFastUnlockAll (
+    IN PFILE_OBJECT FileObject,
+    PEPROCESS ProcessId,
+    OUT PIO_STATUS_BLOCK IoStatus,
+    IN PDEVICE_OBJECT DeviceObject
+    )
+{
+	return TRUE;
+}
+
+BOOLEAN
+FuseFastUnlockAllByKey (
+    IN PFILE_OBJECT FileObject,
+    PVOID ProcessId,
+    ULONG Key,
+    OUT PIO_STATUS_BLOCK IoStatus,
+    IN PDEVICE_OBJECT DeviceObject
+    )
+{
+	return TRUE;
+}
+
+NTSTATUS
+FuseAcquireForCcFlush (
+    IN PFILE_OBJECT FileObject,
+    IN PDEVICE_OBJECT DeviceObject
+    )
+{
+    return STATUS_CLEANER_CARTRIDGE_INSTALLED;
+}
+
+NTSTATUS
+FuseReleaseForCcFlush (
+    IN PFILE_OBJECT FileObject,
+    IN PDEVICE_OBJECT DeviceObject
+    )
+{
+	return STATUS_CLEANER_CARTRIDGE_INSTALLED;
+}
+
