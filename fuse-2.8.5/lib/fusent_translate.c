3d20cca04b80d271f185b553982bd56a2dfe073a
konrad <konrad@0ad51bf3-3eb5-41bf-9b2c-be99ea3c4cf2>
2011-05-03 07:37:20 +0000

    fusent_translate.c: Add more conversion stuff. However, I'm thinking this needs to be slightly different in structure -- the translation layer needs to be able to issue many FUSE commands. Perhaps rather than translate the buffers directly, it should instead interpret and dispatch.
    git-svn-id: file:///home/konrad/curqtr/cse481a/schooldir/repo@30 0ad51bf3-3eb5-41bf-9b2c-be99ea3c4cf2

--- fuse-2.8.5/lib/fusent_translate.c
+++ fuse-2.8.5/lib/fusent_translate.c
@@ -1,5 +1,7 @@
 #ifdef __CYGWIN__
 
+#include <iconv.h>
+
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -9,18 +11,18 @@
 #include "fusent_proto.h"
 #include "fusent_irpdecode.h"
 
-// To get this to compile before I'm finished, uncomment the following:
-//#define XXX 0xdeadbeef
-
 // Sets up any data structures fusent_translate will need to persist
 // across calls.
+iconv_t cd_utf16le_to_utf8;
 void fusent_translate_setup()
 {
+	cd_utf16le_to_utf8 = iconv_open("UTF-8//IGNORE", "UTF-16LE");
 }
 
 // Destroys any persistant data structures at shut down.
 void fusent_translate_teardown()
 {
+	iconv_close(cd_utf16le_to_utf8);
 }
 
 // Translates a FUSENT kernel request (`req') into a FUSE request, filling in
@@ -38,9 +40,13 @@ int fusent_translate_nt_to_fuse(FUSENT_REQ *req, char *outbuf, int *sz)
 	status = fusent_decode_irp(&req->irp, &req->iostack[0], &irptype, &iosp);
 	if (status < 0) return status;
 
+	struct fuse_in_header *header = (struct fuse_in_header *)outbuf;
+	void *arguments = outbuf + sizeof(struct fuse_in_header);
+	*sz = sizeof(struct fuse_in_header);
+
 	switch (irptype) {
 		case IRP_MJ_CREATE:
-			// TODO: Some of the behavior here probably doesn't match
+			// TODO(cemeyer): Some of the behavior here probably doesn't match
 			// one-to-one with NT. Does anyone actually use
 			// FILE_SUPERSEDE? We ignore a lot of CreateOptions flags.
 
@@ -50,47 +56,50 @@ int fusent_translate_nt_to_fuse(FUSENT_REQ *req, char *outbuf, int *sz)
 			uint8_t CreateDisp = CreateOptions >> 24;
 
 			int fuse_flags = 0, mode = S_IRWXU | S_IRWXG | S_IRWXO;
-
 			if (CreateDisp != FILE_OPEN && CreateDisp != FILE_OVERWRITE)
 				fuse_flags |= O_CREAT;
-
 			if (CreateDisp == FILE_CREATE) fuse_flags |= O_EXCL;
-			
 			if (CreateDisp == FILE_OVERWRITE || CreateDisp == FILE_OVERWRITE_IF ||
 					CreateDisp == FILE_SUPERSEDE)
 				fuse_flags |= O_TRUNC;
-
 			if (CreateOptions & FILE_DIRECTORY_FILE) fuse_flags |= O_DIRECTORY;
 
-			*sz = 0;
-			struct fuse_in_header *fih = (struct fuse_in_header *)outbuf;
-			fih->opcode = XXX;
-			fih->unique = XXX;
-			fih->nodeid = XXX;
-			fih->uid = 0;		// TODO: do these even have meaningful values?
-			fih->gid = 0;
-			fih->pid = getpid();
+			header->unique = 0;		// as far as I can tell, this is only used for interrupting ops
+			header->nodeid = XXX;		// inode number
+			header->uid = 0;		// TODO: do these even have meaningful values?
+			header->gid = 0;
+			header->pid = getpid();
 
 			uint32_t fnamelen;
 			uint16_t *fnamep;
 			fusent_decode_request_create((FUSENT_CREATE_REQ *)req,
 					&fnamelen, &fnamep);
-			// TODO: Now that we have the filename, where do we put it?
-			// Do we need to translate UTF-16LE to UTF-8?
 
 			if (fuse_flags & O_CREAT) {
-				struct fuse_create_in *fc = (struct fuse_create_in *)(fih + 1);
+				header->opcode = FUSE_CREATE;
+				struct fuse_create_in *fc = (struct fuse_create_in *)arguments;
+				*sz += sizeof(struct fuse_create_in);
+
+				char *inbuf = fnamep;
+				size_t inbytes = fnamelen, outbytes = 255;
+				char *outbuf = PARAM(fc);
+
+				// Encode the UTF-16LE name from windows into UTF-8
+				iconv(cd_utf16le_to_utf8, &inbuf, &inbytes, &outbuf, &outbytes);
+
 				fc->flags = fuse_flags;
 				fc->mode = mode;
-				fc->umask = XXX;
-				// TODO: verify this is the intended purpose of ->len field:
-				*sz = fih->len = sizeof(struct fuse_in_header) +
-					sizeof(struct fuse_create_in);
+				fc->umask = S_IWGRP | S_IWOTH;
+
+				*sz += (255 - outbytes); // size of utf-8 name
+				header->len = *sz;
 			}
 			else {
-				struct fuse_open_in *fo = (struct fuse_open_in *)(fih + 1);
-				//fo->fh = XXX;
-				//fo->open_flags = fuse_flags;
+				header->opcode = FUSE_OPEN;
+				struct fuse_open_in *fo = (struct fuse_open_in *)arguments;
+				*sz += sizeof(struct fuse_open_in);
+
+				fo->flags = fuse_flags;
 				// TODO: verify this is the intended purpose of ->len field:
 				*sz = fih->len = sizeof(struct fuse_in_header) +
 					sizeof(struct fuse_open_in);
