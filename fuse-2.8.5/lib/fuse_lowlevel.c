4b27b491423c197bfa72097aa5ea225e644b2259
konrad <konrad@0ad51bf3-3eb5-41bf-9b2c-be99ea3c4cf2>
2011-05-29 00:00:30 +0000

    lib/fuse_lowlevel.c: Nuke EWOULDBLOCK since it's the same as POSIX.1 EAGAIN on Cygwin.
    git-svn-id: file:///home/konrad/curqtr/cse481a/schooldir/repo@110 0ad51bf3-3eb5-41bf-9b2c-be99ea3c4cf2

--- fuse-2.8.5/lib/fuse_lowlevel.c
+++ fuse-2.8.5/lib/fuse_lowlevel.c
@@ -1696,9 +1696,7 @@ static inline NTSTATUS fusent_translate_errno(int err)
 		case EEXIST: return STATUS_CANNOT_MAKE;
 		case ENOSPC: break;
 		case ENOSYS: return STATUS_NOT_IMPLEMENTED;
-
-		//case EAGAIN: EAGAIN and EWOULDBLOCK turn out to be the same code, so this case would duplicate the subsequent case statement.
-		case EWOULDBLOCK: return STATUS_RETRY;
+		case EAGAIN: return STATUS_RETRY;
 	}
 
 	return STATUS_UNSUCCESSFUL;
